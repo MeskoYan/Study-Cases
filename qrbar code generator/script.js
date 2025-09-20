1ff5d2d4a495c58231f73d1239b31115a1829bfb
Yan Mesko <yanmesko@gmail.com>
2025-09-20 10:24:41 -0300

    Progress in the QRCode understanding and index files

--- qrbar code generator/script.js
+++ qrbar code generator/script.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded', () =>{
     const text = document.getElementById('user_text');
     console.log(text);
+    let qrcode = new QRcode(document.getElementById("qrcode"),{
+        text: "https://www.youtube.com/",
+        width: 128,
+        height: 128
+    });
 })
