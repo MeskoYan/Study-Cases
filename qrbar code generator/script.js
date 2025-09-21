93e21998b1a18cd96c657818d4f9430857aca059
Yan Mesko <yanmesko@gmail.com>
2025-09-20 21:14:11 -0300

    generation done, but style needs improvements

--- qrbar code generator/script.js
+++ qrbar code generator/script.js
@@ -1,9 +1,15 @@
-document.addEventListener('DOMContentLoaded', () =>{
-    const text = document.getElementById('user_text');
-    console.log(text);
-    let qrcode = new QRcode(document.getElementById("qrcode"),{
-        text: "https://www.youtube.com/",
-        width: 128,
-        height: 128
+document.addEventListener('DOMContentLoaded', ()=>{
+    const input = document.getElementById("input");
+    const display = document.getElementById("display");
+    const button = document.getElementById("generate_code");
+    display.style.position = "absolute";
+    display.style.visibility = "hidden";
+    input.value = "";
+
+    button.addEventListener("click", () =>{
+        let input = document.getElementById("input");
+        display.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + input.value;
+        display.style.position = "relative";
+        display.style.visibility = "visible"
     });
 })
